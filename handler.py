bcc9868001b45682f4465fe0b4e4fea7493f1396
aaishanz <aaishanz.361@gmail.com>
2026-07-09 11:57:28 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import runpod
 import requests
-from PIL import Image # Adding Pillow support
+import torch
+from PIL import Image
 
-print("Status: RunPod worker started with Pillow support.")
+print("Status: RunPod worker started with Torch.")
 
 def handler(job):
     try:
@@ -10,10 +11,12 @@ def handler(job):
 
         return {
             "status": "success",
-            "message": "Pillow loaded successfully!",
-            "pillow_version": Image.__version__, # Verifies Pillow is working
+            "torch_version": torch.__version__,
+            "cuda_available": torch.cuda.is_available(),
+            "cuda_device_count": torch.cuda.device_count(),
             "received_input": job_input
         }
+
     except Exception as e:
         return {
             "status": "error",
